3b92fe6e282493037ab199e0dca4645d10a08609
dnthanksg <dnteague@ncsu.edu>
2019-08-02 23:17:01 -0400

    Added timer

---
+++ Assets/Scripts/AddScore.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddScore : MonoBehaviour
+{
+
+    public Text popUpScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void scoreAdded(int score) {
+        print("score added");
+        popUpScore.text = "+" + score.ToString() + "\n";
+        //popUpScore.
+        //FadeTextToFullAlpha(2f, popUpScore);
+    }
+
+    public IEnumerator FadeTextToFullAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+        FadeTextToZeroAlpha(t, i);
+    }
+ 
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+}
