061db5a497d731c57ee572a255fec92a1c679514
Dominic Brown <domingomonk@gmail.com>
2019-08-02 22:36:46 -0400

    Added bomb throwing

--- Assets/Scripts/FPSPlayerScript.cs
+++ Assets/Scripts/FPSPlayerScript.cs
@@ -7,7 +7,10 @@ public class FPSPlayerScript : MonoBehaviour
 {
     public Rigidbody rb;
     public GameObject pov;
-    public GameObject bombObject;
+    public GameObject bombProp;
+    public GameObject bombThrowable;
+
+    private bool holdingBomb = true;
 
     public float walkSpeed = 15.0F;
     public float sprintSpeed = 150.0F;
@@ -16,25 +19,65 @@ public class FPSPlayerScript : MonoBehaviour
     public float xMouseSensitivity = 10;
     public float yMouseSensitivity = 10;
 
+    public float bombThrowForce = 10.0F;
+
+    public float tossMagnitude = 20.0F;
+
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
 
         if(!rb) Debug.LogError("Could not find Rigidbody", this);
         if (!pov) Debug.LogError("Could not find POV", this);
-        if (!bombObject) Debug.LogError("Could not find BombObject", this);
+        if (!bombProp) Debug.LogError("Could not find BombObject", this);
     }
 
     void Update()
     {
         CheckPlayerMovement();
-        //CheckSetBomb();
+        InteractWithBomb();
+
+        // TODO: Check Detenate Bomb
     }
 
-    private void CheckSetBomb()
+    private void InteractWithBomb()
     {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Debug.Log("Click!");
+            if (holdingBomb)
+            {
+                ThrowBomb();
+            }
+            else
+            {
+                TryPickingUpBomb();
+            }
+            
+        }
         // Click to set bomb on surface
+        // If it's close enough, it should be placed on the floor or attached to its side
+        // If it's not close enough, don't do anything
+        
+        // If bomb is clicked on, pick back up
         throw new NotImplementedException();
+    }
+
+    private void ThrowBomb()
+    {
+        Vector3 tossVector = tossMagnitude * Vector3.up;
+        GameObject thrownBomb = Instantiate(bombThrowable, bombProp.transform.position, this.transform.rotation);
+        thrownBomb.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * bombThrowForce);
+        thrownBomb.GetComponent<Rigidbody>().AddRelativeForce(tossVector);
+
+        bombProp.SetActive(false);
+        holdingBomb = false;
+    }
+
+    private bool TryPickingUpBomb()
+    {
+        throw new NotImplementedException();
+        //holdingBomb = true;
     }
 
     private void CheckPlayerMovement()
